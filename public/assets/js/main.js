310573d6ec4d89d212c46f4a71b96af95795b275
m.liwen <liwen.marcin@gmail.com>
2024-01-04 00:41:53 +0100

    add animations

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -124,7 +124,7 @@ window.onload = () => {
                 markers: false,
                 trigger: "#top",
                 start: "top-=121", // when the top of the trigger hits the top of the viewport
-                end: isMobile?  "bottom": "bottom -=900", // end after scrolling 500px beyond the start
+                end: isMobile ? "bottom" : "bottom -=900", // end after scrolling 500px beyond the start
                 scrub: 1,
                 toggleAction: 'play none none reverse',
                 pin: false, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
@@ -138,16 +138,16 @@ window.onload = () => {
             }
         })
         tl.to('.mask-img', {
-            scaleX: isMobile ?'4':'3',
-            scaleY: isMobile ?'4':'3',
+            scaleX: isMobile ? '4' : '3',
+            scaleY: isMobile ? '4' : '3',
             z: '1',
-            y: isMobile ? '-40vh': '0',
+            y: isMobile ? '-40vh' : '0',
         })
 
 
-            
-        
-        
+
+
+
         var bg = gsap.timeline({
             scrollTrigger: {
                 markers: false,
@@ -161,16 +161,37 @@ window.onload = () => {
         bg.to('.video-cover', {
             opacity: '0.7',
             duration: '1'
-            
-        })
-       
 
-        const lenis = new Lenis()
+        })
+        const lenis = new Lenis({
+            duration: 3,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            smoothTouch: false,
+            touchMultiplier: 2,
+          })
+          function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+          }
+          requestAnimationFrame(raf)
+
+       /*  const lenis = new Lenis({
+            duration: 2,
+            easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            smoothTouch: false,
+            touchMultiplier: 2,
+        })
         lenis.on('scroll', ScrollTrigger.update)
         gsap.ticker.add((time) => {
-            lenis.raf(time * 700)
+            lenis.raf(time * 2000)
         })
-        gsap.ticker.lagSmoothing(0) 
+        gsap.ticker.lagSmoothing(0) */
 
         gsap.set('.second-up', {
             scale: '0.7',
@@ -189,7 +210,7 @@ window.onload = () => {
                 scrub: true,
             }
         })
-        
+
         secondUp.to('.second-up',
             {
                 opacity: '1',
@@ -213,23 +234,30 @@ window.onload = () => {
             scale: '1',
             duration: '1'
         })
-        gsap.from('.line-up',
-            {
-                scrollTrigger: '.line-up',
-                duration: 0.5,
-                opacity: 0,
+
+        gsap.set('.line-up',{
+                 opacity: 0,
                 rotationX: -100,
                 force3D: true,
                 transformOrigin: "top center -100",
+        })
+        gsap.to('.line-up',
+            {
+                scrollTrigger: '.line-up',
+                duration: 0.5,
+                opacity: 1,
+                rotationX: 0,
+                force3D: true,
+                //transformOrigin: "top center -100",
                 stagger: 0.2
             })
 
         var sections = gsap.utils.toArray('.scale-up');
         sections.forEach((section) => {
             let tl = gsap.timeline({
-                onStart: ()=>{
+                onStart: () => {
                     let opacityFull = gsap.timeline({
-                        scrollTrigger:{
+                        scrollTrigger: {
                             markers: false,
                             trigger: '.video-cover',
                             start: 'bottom',
@@ -237,7 +265,7 @@ window.onload = () => {
                             scrub: true
                         }
                     })
-                    opacityFull.to('.video-cover',{
+                    opacityFull.to('.video-cover', {
                         opacity: '1'
                     })
                 },
@@ -249,7 +277,7 @@ window.onload = () => {
                     scrub: true,
                 }
             })
-            
+
             tl.from(section, {
                 scale: '0.8',
             });
@@ -266,9 +294,9 @@ window.onload = () => {
                     scrub: true,
                 }
             })
-            
+
             tl.from(project, {
-             
+
             });
         })
 
@@ -281,18 +309,43 @@ window.onload = () => {
                     end: 'top =50%',
                     markers: false,
                     scrub: true,
-                    
+
                 }
             })
             tl.addLabel('start')
-            .from(slideUp, {
-                y: '15vw',
-            });
+                .from(slideUp, {
+                    y: '15vw',
+                });
         })
+
+        let mm = gsap.matchMedia();
+        mm.add("(max-width: 1024px)", () => {
+            var mainBtns = gsap.utils.toArray('.main-btn');
+            mainBtns.forEach((mainBtn)=>{
+               gsap.to(mainBtn, {
+              
+                duration: 0.7,
+                paddingRight: '0px',
+               });
+               gsap.to(mainBtn.querySelector('.point'),{
+                duration: 0.7,
+                background: '#fff',
+                right: '14px'
+               });
+               gsap.to(mainBtn.querySelector('.main-text'),{
+                duration: 0.7,
+                background: '#8d95f9',
+                borderColor: '#8d95f9',
+                color: '#1823A7',
+                paddingRight: '44px'
+               })
+            })
+        })
+        
         const isTouchDevice = 'ontouchstart' in window;
         const createCursorFollower = () => {
             const el = document.querySelector('.cursor-follower');
-            if(el){
+            if (el) {
                 window.addEventListener('mousemove', (e) => {
                     const { target, x, y } = e;
                     const isTargetLinkOrBtn = target?.closest('.cursor-grab');
@@ -304,7 +357,7 @@ window.onload = () => {
                     })
                 });
             }
-           
+
 
             document.querySelector('.cursor-grab')?.addEventListener('mouseenter', (e) => {
                 gsap.to(el, {
@@ -330,25 +383,25 @@ window.onload = () => {
 
     let counterTargets = document.querySelectorAll(".counterUp");
 
-  let observerCounter = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("is_visible");
-        setTimeout(() => {
-          letCount();
-        }, 1000);
-      }
-      if (entry.isIntersecting) {
-        observer.unobserve(entry.target);
-      }
+    let observerCounter = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is_visible");
+                setTimeout(() => {
+                    letCount();
+                }, 1000);
+            }
+            if (entry.isIntersecting) {
+                observer.unobserve(entry.target);
+            }
+        });
     });
-  });
 
-  if (counterTargets.length > 0) {
-    counterTargets.forEach((target) => {
-      observerCounter.observe(target);
-    });
-  }
+    if (counterTargets.length > 0) {
+        counterTargets.forEach((target) => {
+            observerCounter.observe(target);
+        });
+    }
 }
 
 /**
@@ -358,32 +411,32 @@ window.onload = () => {
 function letCount() {
     const counters = document.querySelectorAll(".counterUp");
     const speed = 100; // The lower the slower
-  
+
     counters.forEach((counter) => {
-      const updateCount = () => {
-        const target = +counter.getAttribute("data-target");
-       
-        const count = +counter.innerText;
-  
-        // Lower inc to slow and higher to slow
-  
-        const inc = target > speed ? target / speed : 1;
-  
-        // Check if target is reached
-        if (count < target) {
-          // Add inc to count and output in counter
-         let num = Math.round(count + inc);
-         console.log(num.toLocaleString())
-          counter.innerText = num ;
-          // Call function every ms
-          setTimeout(updateCount, 50);
-        } else {
-          counter.innerText = target.toLocaleString();
-        }
-      };
-  
-      updateCount();
+        const updateCount = () => {
+            const target = +counter.getAttribute("data-target");
+
+            const count = +counter.innerText;
+
+            // Lower inc to slow and higher to slow
+
+            const inc = target > speed ? target / speed : 1;
+
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter
+                let num = Math.round(count + inc);
+                console.log(num.toLocaleString())
+                counter.innerText = num;
+                // Call function every ms
+                setTimeout(updateCount, 50);
+            } else {
+                counter.innerText = target.toLocaleString();
+            }
+        };
+
+        updateCount();
     });
-  
-  
-  }
+
+
+}
